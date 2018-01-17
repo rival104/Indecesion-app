aa1534a248bc5f4715f605ea6e7ff24a1982724e
rival104 <navid.rvl@gmail.com>
2018-01-16 22:03:11 -0500

    finish making react components

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,21 +1,51 @@
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handelToggleVisibility = this.handelToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        }
+    }
+    handelToggleVisibility() {
+        this.setState((prevState) => {
+            return{
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render() {
+        return(
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handelToggleVisibility}>
+                    {this.state.visibility ? 'Hide details' : 'Show Details'}
+                </button>
+                {this.state.visibility && <p>It's working without page refresh!</p>}
+            </div>
+        );
+    }
+}
 
-let visibilty = false;
-const toggle = () => {
-    visibilty = visibilty === false ? true : false;
-    reRender();
-};
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-const reRender = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggle}>
-                {visibilty ? 'Hide Details': 'Show Details'}
-            </button>
-            {visibilty && <p>It's working without page refresh!</p>}
-        </div>
-    );
-    ReactDOM.render(template, document.getElementById('app'));
-};
 
-reRender();
+// let visibilty = false;
+// const toggle = () => {
+//     visibilty = visibilty === false ? true : false;
+//     reRender();
+// };
+
+// const reRender = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggle}>
+//                 {visibilty ? 'Hide Details': 'Show Details'}
+//             </button>
+//             {visibilty && <p>It's working without page refresh!</p>}
+//         </div>
+//     );
+//     ReactDOM.render(template, document.getElementById('app'));
+// };
+
+// reRender();
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,34 +1,79 @@
-let count = 0;
-const addOne = () => {
-    count++;
-    reRenderCount();
-    console.log('addOne');
-};
-const minusOne = () => {
-    count--;
-    reRenderCount();
-    console.log('minusOne');
-};
-const reset = () => {
-    count = 0;
-    reRenderCount();
-    console.log('reset');
-};
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handeleAddOne = this.handeleAddOne.bind(this);
+        this.handeleMinusOne = this.handeleMinusOne.bind(this);
+        this.handeleReset = this.handeleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
+    handeleAddOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            };
+        });
+    }
+    handeleMinusOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            };
+        });
+    }
+    handeleReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            };
+        }); 
+    }
+    render() {
+        return(
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handeleAddOne}>+1</button>
+                <button onClick={this.handeleMinusOne}>-1</button>
+                <button onClick={this.handeleReset}>reset</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     reRenderCount();
+//     console.log('addOne');
+// };
+// const minusOne = () => {
+//     count--;
+//     reRenderCount();
+//     console.log('minusOne');
+// };
+// const reset = () => {
+//     count = 0;
+//     reRenderCount();
+//     console.log('reset');
+// };
 
 
 
-const reRenderCount = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-    const appRoot = document.getElementById("app");
+// const reRenderCount = () => {
+//     const templateTwo = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>reset</button>
+//         </div>
+//     );
+//     const appRoot = document.getElementById("app");
 
-    ReactDOM.render(templateTwo, appRoot);
-};
+//     ReactDOM.render(templateTwo, appRoot);
+// };
 
-reRenderCount();
+// reRenderCount();
